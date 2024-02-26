8682c14525f1410d38a8189491b8e01055c2893b
Alberto Paim <albertoopaim@gmail.com>
2024-02-26 10:22:47 -0300

    reduce

--- react_studies/src/app/page.tsx
+++ react_studies/src/app/page.tsx
@@ -3,16 +3,54 @@
 import { useEffect, useState } from "react";
 import { VideoPlayer } from "./components/VideoPlayer";
 import { Square } from "./components/Square";
+import { Item } from "./types/Item";
 
 const Page = () => {
-  const [show, setShow] = useState(false);
 
+  const [list, setList] = useState<Item[]>([])
+
+  const addNewItem = (text: string) => {
+    setList([...list, {
+      id: list.length,
+      text,
+      done: false
+    }]);
+  }
+
+  const editiItemText = (id: number, newText: string) => {
+    setList(
+      list.map(item => {
+        if (item.id === id) {
+          item.text = newText
+        }
+        return item;
+      })
+    )
+  }
+
+  const toggleItemStatus = (id: number) => {
+    setList(
+      list.map(item => {
+        if (item.id === id) item.done = !item.done
+
+        return item;
+      })
+    )
+  }
+
+
+  const deleteItem = (id: number) => {
+    setList(
+      list.filter(item => {
+        return item.id !== id
+      })
+    )
+  }
   return (
     <main className="h-screen w-screen  flex justify-center items-center flex-col gap-10">
-      {show && <Square></Square>}
-      <button onClick={() => setShow(!show)}> Mostrar/Esconder</button>
+
     </main>
   )
-}
+};
 
 export default Page;
